0523ff6b2cc3fea3ae255900f1ed10aa016fb3ae
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2025-02-13 22:54:59 +0100

    Remove unused import from user_auth_app.models.py

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,5 +1,4 @@
 
-from typing import Required
 from django.contrib.auth.models import AbstractBaseUser,  PermissionsMixin
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
