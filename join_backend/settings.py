996e6aaa0c192693550453ec6c6f627c35eae130
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2025-02-17 01:22:52 +0100

    Configure Whitenoise to use CompressedManifestStaticFilesStorage for optimized static file handling

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'join_backend.urls'
 
 TEMPLATES = [
