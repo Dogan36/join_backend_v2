3e71767d090d857c173fec264319f49676a01823
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2025-02-17 01:10:15 +0100

    Add join_backend_v2 to sys.path in manage.py for module accessibility

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    sys.path.append(os.path.join(os.path.dirname(__file__), "join_backend_v2"))
     execute_from_command_line(sys.argv)
 
 
