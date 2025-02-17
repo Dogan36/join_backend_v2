e65185fb05fc9576d6bae6f1c922b156d3414703
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2025-02-17 02:44:55 +0100

    Fix formatting issue in DATABASES setting in settings.py

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     )
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
